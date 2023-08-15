770258c174819caac48bd94762e8a39e25f6b00d
Yamila266 <yami_r00@hotmail.com>
2023-08-15 19:44:15 -0300

    ejerciciocon error

---
+++ ejercicio15-08.py
@@ -0,0 +1,46 @@
+print("1- Ingresar valor 1\n")
+b=(input())
+print("2- Ingresar valor 2\n")
+C=(input())
+
+print("ingrese una opción: \n")
+
+print("3- Mostrar Suma\n")
+print("4- Mostrar Resta\n")
+print("5- Mostrar Multiplicación\n")
+print("6- Mostrar División\n")
+print("7- Salir\n")
+a=(input())
+while a!=7:
+    if a==1:
+        print("Ingrese Valor 1")
+        b= (input())
+    if a==2:
+        print("Ingrese Valor 2")
+        c= (input())
+    if a==3:
+        print("Resultado de la suma")
+        suma=a+b
+        print(suma)
+    if a==4:
+        print("Resultado de la resta")
+        resta=a-b
+        print(resta)
+    if a==5:
+        print("Resultado de la multiplicación")
+        mult=a*b
+        print(mult)
+    if a==6:
+        print("Resultado de la división")
+        div=a/b
+        print(div)
+    print("\n Ingrese una opción: \n")
+    print("1- Ingresar valor 1\n")
+    print("2- Ingresar valor 2\n")
+    print("3- Mostrar Suma\n")
+    print("4- Mostrar Resta\n")
+    print("5- Mostrar Multiplicación\n")
+    print("6- Mostrar División\n")
+    print("7- Salir\n")
+    a=(input())    
+print("CHAO CHAO")
